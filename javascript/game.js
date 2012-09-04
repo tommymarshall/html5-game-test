01d97393c5e6971b11c5aa4b3e6fa82f424f2841
Tommy Marshall <tommy.marshall@viget.com>
2012-09-04 12:52:04 -0400

    Added spaceship. Added more .js.

--- javascript/game.js
+++ javascript/game.js
@@ -5,26 +5,84 @@ var SAY = SAY || {};
 	var _game = SAY.viget = {
 
 		init: function() {
-			_game.drawCanvas();
+			_game.setVars();
+			_game.setUpCanvas();
+
+			_game.Enemies.create();
+			_game.Ship.create();
+
+			_game.start();
 		},
 
-		Layers: {
+		start: function() {
+			_game.render();
+		},
 
-			add: function(){
+		setVars: function() {
+			_game.state = 'start';
+			
+			_game.keyboard = {};
 
+			_game.player = {
+				x: 500,
+				y: 900,
+				width: 50,
+				height: 72,
+				counter: 0
+			};
+			_game.playerBullets = [];
+			
+			_game.enemies = [];
+			_game.enemyBullets = [];
+		},
+
+		Enemies: {
+			create: function(){
+				for(var i in _game.enemies) {
+						var enemy = _game.enemies[i];
+						if(enemy.state == "alive") {
+								_game.ctx.fillStyle = "green";
+								_game.ctx.fillRect(enemy.x,enemy.y,enemy.width,enemy.height);
+						}
+						if(enemy.state == "hit") {
+								_game.ctx.fillStyle = "purple";
+								_game.ctx.fillRect(enemy.x,enemy.y,enemy.width,enemy.height);
+						}
+						//this probably won't ever be called.
+						if(enemy.state == "dead") {
+								_game.ctx.fillStyle = "black";
+								_game.ctx.fillRect(enemy.x,enemy.y,enemy.width,enemy.height);
+						}
+				}
 			}
 		},
 
-		drawCanvas: function(){
+		Ship: {
+			create: function(){
+				_game.ship = new Image();
+				_game.ship.src = '../images/spaceship.png';
+			},
+
+			show: function() {
+				_game.ctx.drawImage(_game.ship, 0, 0);
+			}
+		},
+
+		setUpCanvas: function(){
 			_game.canvas = document.createElement( 'canvas' );
-			_game.ctx		= _game.canvas.getContext( '2d' );
 			_game.canvas.width	= 1280;
 			_game.canvas.height = 1024;
 
 			$( '.canvas' ).html(_game.canvas);
 
+			_game.ctx = _game.canvas.getContext( '2d' );
+			_game.ctx.fillStyle = '#000';
+			_game.ctx.fillRect(0,0,1280,1024);
+		},
+
+		render: function() {
 			function render() {
-				_game.Particles.run();
+				_game.Ship.show();
 				window.requestAnimFrame(render);
 			}
 
@@ -41,79 +99,6 @@ var SAY = SAY || {};
 			})();
 
 			render();
-
-			$('button').toggle(function(){
-				_game.Particles.play = false;
-			},function(){
-				_game.Particles.play = true;
-			});
-		},
-
-		Particles: {
-			part: [],
-			tick: 0,
-			play: true,
-
-			run: function(){
-				var _this = _game.Particles;
-				if (_this.play === true) {
-					_game.Particles.create();
-					_game.Particles.update();
-					_game.Particles.kill();
-					_game.Particles.draw();
-				}
-			},
-			create: function(){
-				var _this = _game.Particles;
-
-				//check on every 10th tick check
-				if(_this.tick % 20 === 0) {
-						//add particle if fewer than 100
-						if(_this.part.length < 100) {
-								_this.part.push({
-									x: Math.random() * _game.canvas.width, //between 0 and canvas width
-									y: 0,
-									speed: 2+Math.random() * 3, //between 2 and 5
-									radius: 5+Math.random() * 5, //between 5 and 10
-									color: "white"
-								});
-						}
-				}
-			},
-			update: function(){
-				var _this = _game.Particles;
-
-				for(var i in _this.part) {
-						var part = _this.part[i];
-						part.y += part.speed;
-				}
-
-				_game.Particles.tick++;
-			},
-			kill: function(){
-				var _this = _game.Particles;
-
-				for(var i in _this.part) {
-					var part = _this.part[i];
-					if(part.y > _game.canvas.height) {
-						part.y = 0;
-					}
-				}
-			},
-			draw: function(){
-				var _this = _game.Particles;
-
-				_game.ctx.fillStyle = "black";
-				_game.ctx.fillRect(0,0,_game.canvas.width,_game.canvas.height);
-				for(var i in _this.part) {
-					var part = _this.part[i];
-					_game.ctx.beginPath();
-					_game.ctx.arc(part.x,part.y, part.radius, 0, Math.PI*2);
-					_game.ctx.closePath();
-					_game.ctx.fillStyle = part.color;
-					_game.ctx.fill();
-				}
-			}
 		}
 	};
 
